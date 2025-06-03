4da71c93e103232aa61d890abfa52ee76452514a
smolito <smolito27@gmail.com>
2025-06-03 03:32:31 +0200

    added custom 404

--- postavy/postavy/settings.py
+++ postavy/postavy/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d1o&i9(vtf=c$beq59bbof#ua728mjk=b_hnd7ll67x$*%ohh6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
